94a589242b51ea2e0acce239588702ca3a90ba6a
shubhamjain31 <sj27754@gmail.com>
2022-10-07 20:51:47 +0530

    change database in settings file

--- core/settings.py
+++ core/settings.py
@@ -78,27 +78,27 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE':   'django.db.backends.postgresql_psycopg2',
-#         'NAME':     'assignment',
-#         'USER':     'postgres',
-#         'PASSWORD': 'root',
-#         'HOST':     'localhost',
-#         'PORT':     '5432',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE':   'django.db.backends.mysql',
+        'ENGINE':   'django.db.backends.postgresql_psycopg2',
         'NAME':     'assignment',
-        'USER':     'root',
+        'USER':     'postgres',
         'PASSWORD': 'root',
         'HOST':     'localhost',
-        'PORT':     '3306',
+        'PORT':     '5432',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE':   'django.db.backends.mysql',
+#         'NAME':     'assignment',
+#         'USER':     'root',
+#         'PASSWORD': 'root',
+#         'HOST':     'localhost',
+#         'PORT':     '3306',
+#     }
+# }
 
 
 # Password validation
